48b0aea3fea219319f44ddd98301ea6911cf7a94
swiderro <swiderro@52422288-41fb-e3c1-a42a-a39004acd2d8>
2011-03-27 01:22:16 +0000

    Stan na 20110326.

---
+++ trunk/buyingDutchman/BDC.java
@@ -0,0 +1,140 @@
+package buyingDutchman;
+
+import java.util.HashMap;
+
+public final class BDC {
+	//BuyingDutchman Consts
+	public static final String[] AUCTIONSCOLUMNNAMES = {
+		"# aukcji", "Wystawca", "Kategoria", "Podkategoria", "Pozo. czas", "Cena", "Max oferta", "Tytu³"		
+	};
+	public static final String[] FINISHEDAUCTIONSCOLUMNNAMES = {
+		"# aukcji", "Wystawca", "Kategoria", "Podkategoria", "Pozo. czas", "Cena", "Max oferta", "Oferent", "Tytu³"		
+	};
+	public static final int[] AUCTIONSCOLUMNWIDTH = {
+		50, 60, 60, 60, 60, 60, 60, 60, 210
+	};
+	public static final int[] FINISHEDAUCTIONSCOLUMNWIDTH = {
+		50, 60, 60, 60, 60, 60, 60, 70, 200
+	};
+	public static final String BDONTO = "bd-ontology";
+	public static final String SDTYPELISTENER = "bd-agent";
+	public static final String SDNAMELISTENER = "Dutch Auction Agent";	
+	public static final String SDTYPEAUCTION = "bd-auction";
+	public static final String SDNAMEAUCTION = "Agent Dutch Auctions";
+	public static final String SDPNAME = "Dutch Auction";
+	public static final int TICK = 1000;
+	public static final int GUIAUCTION = 0;
+	public static final int GUIBUYNOW = 1;
+	public static final int GUIWAITBUY = 2;
+	public static final int GUICLOSE = 3;
+	public static final int GUIPROPOSITION = 4;
+	public static final float TICKSPERSECOND = 1000/TICK;
+	public static final String PROTOCOLS = "fipa-request fipa-Contract-Net fipa-subscribe fipa-agree fipa-proposal fipa-cfp fipa-accept-proposal";
+	public static final String POSTFIX = ":";
+	public static final String AUCTIONS = "auctions" + POSTFIX;
+	public static final String AUCTION = "auction" + POSTFIX;
+	public static final String PRICESTARTDEF = "0";
+	public static final String DURATIONSTARTDEF = "0";
+	public static final String SEPARATOR = " ";		
+	public static final String POINT = ",";
+	public static final String FPOINT = ".";
+	public static final String FINISHED = "finished" + POSTFIX;
+	public static final int POINTPRECISION = 2;
+	public static final String REGEXPOINT = ",";
+	public static final String REGEXFPOINT = "\\.";
+	public static final String PROPOSEBUYNOW = "propose_buy_now" + POSTFIX;	
+	public static final int BUYNOWAUCTIONEERPARAMNUM = 0;
+	public static final int BUYNOWAUCTIONNRPARAMNUM = 1;
+	public static final String NONESTRING = "-";
+	public static final int WAITBUYAUCTIONNRPARAMNUM = 0;
+	public static final int WAITBUYAUCTIONEERPARAMNUM = 1;
+	public static final int WAITBUYBIDPARAMNUM = 2;
+	public static final int PROPOSEAUCTIONNRPARAMNUM = 0;
+	public static final int PROPOSEAUCTIONEERPARAMNUM = 1;
+	public static final int PROPOSEBIDPARAMNUM = 2;
+	public static final String BID = "BID"+POSTFIX;
+	public static final int AUCTIONPARAMNUM = 0;	
+	//BuyingDutchmanGui Consts
+	//FRAME CONSTS
+	public static final String FRAMETITLE = "Buying Dutchman";  
+	public static final int FRAMEWIDTH = 750;
+	public static final int FRAMEHEIGHT = 400;
+	//TABS CONSTS
+	public static final String AUCTIONSTAB = "Aukcje";
+	public static final String AUCTIONTAB = "Wystaw";
+	//PANELS CONSTS
+	public static final String BIDDERPRICE = "Twoja cena";  
+	//BUTTONS CONSTS
+	public static final String WAITBUYBUTTON = "Oczekuj i kup";
+	public static final String WAITBUYBUTTONLABEL = "Oczekuj i kup";
+	public static final String BUYNOWBUTTON = "Kup teraz";
+	public static final String BUYNOWBUTTONLABEL = "Kup teraz";
+	public static final String PROPOSITIONBUTTON = "Z³ó¿ ofertê";  
+	public static final String PROPOSITIONBUTTONLABEL = "Z³ó¿ ofertê";  
+	//LABELS & TEXTFIELDS CONSTS
+	public static final String DECIMALPOINT = ",";  
+	public static final int PRICEINTCOLUMNS = 4;
+	public static final int PRICEDECCOLUMNS = 2;
+	public static final String STARTINGPRICETEXTFIELD = "Cena wywo³awcza";
+	public static final int DURATIONCOLUMNSNUMBER = 5;
+	public static final String DURATIONHOURSTEXTFIELD = "Godziny";
+	public static final String DURATIONMINUTESTEXTFIELD = "Minuty";
+	public static final String DURATIONSECONDSTEXTFIELD = "Sekundy";	
+	public static final int AUCTIONTITLECOLUMNSNUMBER = 47;
+	public static final String AUCTIONTITLELABEL = "Tytu³ aukcji";
+	public static final String AUCTIONTITLETEXTFIELD = "Tytul aukcji";
+	public static final String MINIMUMPRICELABEL = "Cena minimalna";
+	public static final String AUCTIONDURATIONLABEL = "Czas trwania";
+	public static final String AUCTIONDESCRIPTIONLABEL = "Opis aukcji";
+	public static final String HOURSLABEL = "Godziny";
+	public static final String MINUTESLABEL = "Minuty";
+	public static final String SECONDSLABEL = "Sekundy";
+	public static final String BEGINAUCTION = "Rozpocznij aukcjê";
+	public static final String AUCTIONEER = "Wystawca";
+	public static final int AUCTIONEERCOLUMNS = 5;
+	public static final String AUCTIONNUMBER = "Nr aukcji";
+	public static final int AUCTIONNUMBERCOLUMNS = 4;
+	public static final String FINISHEDAUCTIONSTAB = "Moje aukcje";
+	public static final String CATEGORYLABEL = "Kategoria";
+	public static final String AUCTIONTYPELABEL = "Typ aukcji";
+	//CATEGORIES
+	private static final String ANTIQUES = "Antyki";
+	private static final String COLLECTIONS = "Kolekcje";
+	private static final String FLOWERS = "Kwiaty";
+	private static final String GROCERIES = "Art. spo¿.";
+	private static final String TRANSMISION = "Przesy³";
+	public static final String[] CategoryList = {
+		FLOWERS, GROCERIES, TRANSMISION, ANTIQUES, COLLECTIONS
+	};
+	public static final String SUBCATEGORYLABEL = "Podkategoria";
+	private static HashMap CATEGORY_SUBCATEGORY;
+	private static HashMap getCategorySubcategory() {
+		if (CATEGORY_SUBCATEGORY == null) {
+			CATEGORY_SUBCATEGORY = new HashMap();
+			String[] antiques = {"Meble", "Sztuka", "Zastawa"};
+			CATEGORY_SUBCATEGORY.put(ANTIQUES, antiques);
+			String[] flowers = {"Tulipany", "Ró¿e", "Lilie"};
+			CATEGORY_SUBCATEGORY.put(FLOWERS, flowers);
+			String[] collectables = {"Monety", "Znaczki"};
+			CATEGORY_SUBCATEGORY.put(COLLECTIONS, collectables);
+			String[] groceries = {"Ryby", "Owoce morza", "Warzywa"};
+			CATEGORY_SUBCATEGORY.put(GROCERIES, groceries);
+			String[] transmitables = {"En. elektryczna", "Transfer danych", "Czêstotliwoœæ"};
+			CATEGORY_SUBCATEGORY.put(TRANSMISION, transmitables);
+		}
+		return CATEGORY_SUBCATEGORY;
+	}
+	public static String[] getSubCategoryList(String category) {
+		return (String[]) getCategorySubcategory().get(category);
+	}
+	//AUCTION TYPES
+	private static final String DUTCH = "Holenderska";
+	private static final String ENGLISH = "Angielska";
+	private static final String SECOND_PRICE = "Drugiej ceny";
+	private static final String PRZETARG = "Przetarg";
+	public static final String[] AuctionType = {
+		DUTCH, ENGLISH, SECOND_PRICE, PRZETARG
+	};
+}
+
+
---
+++ trunk/buyingDutchman/LoginGui.java
@@ -0,0 +1,239 @@
+package buyingDutchman;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class LoginGui extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4882352228364389156L;
+	//FRAME CONSTS
+	private static final int FRAMEWIDTH = 310;
+	private static final int FRAMEHEIGHT = 114;
+	private static final String FRAMETITLE = "Buying Dutchman Login";  
+	
+	//ELEMENTS
+	private JPanel jContentPane = null;
+	private JPanel jPAccount = null;
+	private JPanel jPAccountDetails = null;
+	private JPanel jPLogin = null;
+	private JLabel jLLogin = null;
+	private JTextField jTFLogin = null;
+	private JLabel jLHost = null;
+	private JTextField jTFHost = null;
+	private JButton jBLogin = null;
+	//ELEMENTS CONSTS
+	private static final String HOSTLABEL = "Host";
+	private static final String LOGINLABEL = "Login";  //  @jve:decl-index=0:
+	private static final int LOGINCOLUMNSNUMBER = 15;
+	private static final String LOGINTEXTFIELD = "Login";  
+	private static final String LOGINBUTTON = "Zaloguj";  
+	private static final String LOGINTEXTFIELDINFO = "Login info";  
+	//AGENT CONSTS
+	private static final String AGENTNAME = "BuyingDutchman";
+	private static final String SEPARATOR_1 = ":";	
+	private static final String PACKAGENAME = "buyingDutchman";
+	private static final String SEPARATOR_2 = ".";
+	 	
+	/**
+	 * Default constructor 
+	 * 
+	 */
+	public LoginGui() {
+		super();
+		initialize();
+	}
+	
+	/**
+	 * This method initializes this
+	 * 
+	 */
+	private void initialize() {
+        this.setSize(new Dimension(FRAMEWIDTH, FRAMEHEIGHT));
+        this.setContentPane(getJContentPane());
+        this.setTitle(FRAMETITLE);        
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  	  	this.setVisible(true);
+	}
+	
+	/**
+	 * This method initializes jContentPane	
+	 * 	
+	 * @return javax.swing.JPanel	
+	 */
+	private JPanel getJContentPane() {
+		if (jContentPane == null) {
+			jContentPane = new JPanel();
+			jContentPane.setLayout(new BorderLayout());
+			jContentPane.add(getJPAccount(), BorderLayout.CENTER);
+		}
+		return jContentPane;
+	}
+	
+	/**
+	 * This method initializes jPAccount	
+	 * 	
+	 * @return javax.swing.JPanel	
+	 */
+	
+	private JPanel getJPAccount() {
+		if (jPAccount == null) {
+			jPAccount = new JPanel();
+			jPAccount.setLayout(new BorderLayout());
+			jPAccount.add(getJPAccountDetails(), BorderLayout.CENTER);
+			jPAccount.add(getJPLogin(), BorderLayout.NORTH);
+		}
+		return jPAccount;
+	}
+	
+	/**
+	 * This method initializes jPAccountDetails	
+	 * 	
+	 * @return javax.swing.JPanel	
+	 */
+	private JPanel getJPAccountDetails() {
+		if (jPAccountDetails == null) {
+			FlowLayout flowLayout1 = new FlowLayout();
+			flowLayout1.setAlignment(java.awt.FlowLayout.LEFT);
+			jLHost = new JLabel();
+			jLHost.setText(HOSTLABEL);
+			jPAccountDetails = new JPanel();
+			jPAccountDetails.setLayout(flowLayout1);
+			jPAccountDetails.add(jLHost, null);
+			jPAccountDetails.add(getJTFHost(), null);
+		}
+		return jPAccountDetails;
+	}
+	
+	/**
+	 * This method initializes jPLogin	
+	 * 	
+	 * @return javax.swing.JPanel	
+	 */
+	private JPanel getJPLogin() {
+		if (jPLogin == null) {
+			FlowLayout flowLayout = new FlowLayout();
+			flowLayout.setAlignment(java.awt.FlowLayout.LEFT);
+			jLLogin = new JLabel();
+			jLLogin.setText(LOGINLABEL);
+			jPLogin = new JPanel();
+			jPLogin.setLayout(flowLayout);
+			jPLogin.add(jLLogin, null);
+			jPLogin.add(getJTFLogin(), null);
+			jPLogin.add(getJBLogin(), null);
+		}
+		return jPLogin;
+	}
+	
+	/**
+	 * This method initializes jTFLogin	
+	 * 	
+	 * @return javax.swing.JTextField	
+	 */
+	private JTextField getJTFLogin() {
+		if (jTFLogin == null) {
+			jTFLogin = new JTextField(LOGINCOLUMNSNUMBER);
+			jTFLogin.setName(LOGINTEXTFIELD);
+			jTFLogin.addKeyListener(
+	        	new KeyListener() {
+
+					@Override
+					public void keyPressed(KeyEvent arg0) {
+					}
+
+					@Override
+					public void keyReleased(KeyEvent arg0) {
+						if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
+							connect();
+						}
+					}
+
+					@Override
+					public void keyTyped(KeyEvent arg0) {
+					}
+	        		
+	        	}
+	        );
+		}
+		return jTFLogin;
+	}
+
+	/**
+	 * This method initializes jTFLoginInfo	
+	 * 	
+	 * @return javax.swing.JTextField	
+	 */
+	private JTextField getJTFHost() {
+		if (jTFHost == null) {
+			jTFHost = new JTextField(LOGINCOLUMNSNUMBER);
+			jTFHost.setName(LOGINTEXTFIELDINFO);
+			//TODO Delete after testing
+			jTFHost.setText("Marcin-PC");
+		}
+		return jTFHost;
+	}
+
+	/**
+	 * This method initializes jBLogin	
+	 * 	
+	 * @return javax.swing.JButton	
+	 */
+	private JButton getJBLogin() {
+		if (jBLogin == null) {
+			jBLogin = new JButton(LOGINBUTTON);
+			jBLogin.addActionListener( new ActionListener() {
+				public void actionPerformed(ActionEvent ev) {
+					connect();
+				}
+			} );
+		}
+		return jBLogin;
+	}	
+
+	private String[] getBootArguments() {
+		String AgentFQN = getAgentFQN();
+		String [] a = new String[] {
+			"-container"
+			, "-host"
+			, getJTFHost().getText().trim()
+			, AgentFQN
+		};
+		return a;
+	}
+
+	protected void connect() {
+		try {			
+			String login = jTFLogin.getText().trim();
+			if (login != null && login.length() > 0) {
+				try {
+	            	jade.Boot.main(getBootArguments());
+				}
+				catch (Exception ex){
+					ex.printStackTrace();
+				}
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace(); 
+		}
+	}
+	
+	private String getAgentFQN() {
+		return jTFLogin.getText().trim()+SEPARATOR_1+PACKAGENAME+SEPARATOR_2+AGENTNAME;
+	}
+	
+}  //  @jve:decl-index=0:visual-constraint="10,10"
