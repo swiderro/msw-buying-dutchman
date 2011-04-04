27aa989480d9cfbda1ee6de739be3283c49c6ff0
swiderro <swiderro@52422288-41fb-e3c1-a42a-a39004acd2d8>
2011-04-04 21:45:04 +0000

    Auction factory files.

---
+++ buyingDutchman/NullAuction.java
@@ -0,0 +1,9 @@
+package buyingDutchman;
+
+public class NullAuction extends Auction {
+	// TODO Implement according to Null Object Design Pattern
+	public NullAuction(AuctionDetails AD, AuctionItem AI, String AN, String Auctioneer) {
+		super(AD, AI, AN, Auctioneer);
+	}
+
+}
