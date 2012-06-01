96a603af06693709d61e31b16cc15041dc47504d
swiderro <swiderro@52422288-41fb-e3c1-a42a-a39004acd2d8>
2012-06-01 18:51:22 +0000

    Teoria. Dokumentacja JavaDoc. Plan pracy.

---
+++ dokumentacja/clean.bat
@@ -0,0 +1,3 @@
+echo on
+cd c:\
+rd /Q /S C:\Users\Marcin\Uczelnia\magisterka\dokumentacja\java_doc
---
+++ dokumentacja/make.bat
@@ -0,0 +1,3 @@
+echo on
+cd "c:\Users\Marcin\Uczelnia\magisterka\Buying Dutchman\0.2\Buying Dutchman\src"
+javadoc -d C:\Users\Marcin\Uczelnia\magisterka\dokumentacja\java_doc -private -quiet -charset "windows-1250" *
