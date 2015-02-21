b461ec36e9b2161d5729af71e2aa82bb3621566b
swiderro@gmail.com <swiderro@gmail.com@52422288-41fb-e3c1-a42a-a39004acd2d8>
2015-02-21 20:30:49 +0000

    Wydzielenie 2 modułów cz. 1

--- dokumentacja/make.bat
+++
@@ -1,3 +0,0 @@
-echo on
-cd "c:\Users\Marcin\Uczelnia\magisterka\Buying Dutchman\0.2\Buying Dutchman\src"
-javadoc -d C:\Users\Marcin\Uczelnia\magisterka\dokumentacja\java_doc -private -quiet -charset "windows-1250" *
